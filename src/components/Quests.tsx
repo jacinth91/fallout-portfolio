2f411a1def45a9a90fbe45ee02352d5e50137eec
JacinthV <joy237@gmail.com>
2025-11-17 17:28:24 +0530

    Update job entries in Quests component
    Added a new job entry for a Senior Engineer position at Citi Bank, detailing responsibilities and achievements. Updated the Senior Consultant position at Capgemini with new dates and additional details on technologies used.

--- src/components/Quests.tsx
+++ src/components/Quests.tsx
@@ -5,9 +5,27 @@ export const Quests: React.FC = () => (
   <Section>
     <h2>Completed Quests (Experience)</h2>
     <ul>
+       <li>
+        <strong>Senior Engineer Tech at Citi Bank [3/2025–8/2025]</strong>
+        <p>
+          Led the development of a reusable UI component library, published as a
+          private NPM package and hosted in the Nexus repository, enabling
+          multiple teams to integrate it seamlessly into their applications.
+          Built sevrices in Node.js to support the UI library workflows .
+          Additionally worked on a GenAI-powered application for document
+          analysis and summarization using LLMs, improving automation and
+          reducing manual review effort.
+        </p>
+      </li>
       <li>
-        <strong>Senior Consultant at Capgemini [2021-Present]</strong>
-        <p>Led development of enterprise applications using Angular, React, Node.js, and Spring Boot. Implemented CI/CD with Bamboo and deployed on AWS.</p>
+        <strong>Senior Consultant at Capgemini [2021–2025]</strong>
+        <p>
+          Built and maintained enterprise applications using Angular, React,
+          Node.js, and Spring Boot. Worked on LLM-based solutions using Claude
+          on AWS Bedrock for document processing and automation. Managed
+          end-to-end deployment pipelines using jenkins , IBM API connect and
+          delivered scalable cloud-native applications on AWS.
+        </p>
       </li>
       <li>
         <strong>Platform Developer at Pepper HQ [2021]</strong>
